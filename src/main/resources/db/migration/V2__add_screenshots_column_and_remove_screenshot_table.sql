5de7ad96701303274cd449ec178393925c8edbc2
Devin Cooper <devinraycooper@gmail.com>
2025-07-25 15:28:16 -0400

    try and run migrate

--- src/main/resources/db/migration/V2__add_screenshots_column_and_remove_screenshot_table.sql
+++ src/main/resources/db/migration/V2__add_screenshots_column_and_remove_screenshot_table.sql
@@ -2,5 +2,14 @@
 ALTER TABLE project
 ADD COLUMN screenshots TEXT;
 
--- Drop the project_screenshot table
+UPDATE project p
+JOIN (
+    SELECT
+        project_id,
+        GROUP_CONCAT(screenshot ORDER BY id SEPARATOR ',') AS screenshots
+    FROM project_screenshot
+    GROUP BY project_id
+) ps ON p.id = ps.project_id
+SET p.screenshots = ps.screenshots;
+
 DROP TABLE IF EXISTS project_screenshot;
